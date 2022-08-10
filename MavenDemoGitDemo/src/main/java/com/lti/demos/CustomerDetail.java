7d50cf1601cd3deb3588262ca06f2743c434722b
VaibhavSharma <vaibhavsharma7062@gmail.com>
2022-08-10 14:38:36 +0530

    added getter setter and constructor

--- MavenDemoGitDemo/src/main/java/com/lti/demos/CustomerDetail.java
+++ MavenDemoGitDemo/src/main/java/com/lti/demos/CustomerDetail.java
@@ -2,4 +2,25 @@ package com.lti.demos;
 
 public class CustomerDetail {
 
+	private int custId;
+	
+	public CustomerDetail() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerDetail(int custId) {
+		super();
+		this.custId = custId;
+	}
+
+	public int getCustId() {
+		return custId;
+	}
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+
+	
 }
